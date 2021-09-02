9a40e307bee874d3c65452e0e9f29b1edd2c2bd5
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2021-09-02 21:35:05 +0300

    compleate my code

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -11,9 +11,9 @@ class PaymentController extends Controller
 
     public function pay()
     {
-//        $payLink = auth()->user()->charge(14.99, "Pay by paddle");
-//
-//        return view('payments.billing', compact('payLink'));
-        return view('billing');
+        $payLink = auth()->user()->charge(14.99, "Pay by paddle");
+
+        return view('payments.billing', compact('payLink'));
+
     }//end pay function
 }
