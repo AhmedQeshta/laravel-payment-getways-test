16bbc77ae30bb54762c9f2d20410a592305a0b20
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2021-09-02 21:31:55 +0300

    compleate my code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,6 +86,8 @@
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
+<script src="https://payment-getways-test.herokuapp.com/js/app.js"></script>
+
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
     var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
